8069e9acaefa1fc955065cee6860d16ea2e00cfd
Ilya201091 <ZiK201091@ya.ru>
2022-09-04 22:18:46 +0700

    выполнил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public static void checkYear(int year) {
+    if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0)))
+        System.out.println(year + " г. не високосный");
+    else System.out.println(year + " г. високосный");}
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        checkYear(2022);
+    }}
