357075bb0626ea6a9c34042144ff7331c0a266d4
Ilya201091 <ZiK201091@ya.ru>
2022-09-08 15:06:21 +0700

    выполнил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        checkYear(2022);
+        checkYear(2020);
         int clientOS = 0;
         int clientDeviceYear = 2018;
         printRecommendationVersionOs(clientOS, clientDeviceYear);
@@ -10,18 +10,12 @@ public class Main {
         printDeliveryDays(days);
     }
 
-
     // Задача 1
+
     public static void checkYear(int year) {
         if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
             System.out.println(year + " год високосный");
-        else System.out.println(year + " год не високосный");
-    }
-
-//    public static void main(String[] args) {
-//        checkYear(2022);
-//    }
-
+        else System.out.println(year + " год не високосный");}
 
     // Задача 2
 
@@ -34,15 +28,8 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         else if (clientOS == 1 && clientDeviceYear < 2015)
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        else System.out.println("Ваша версия OS не поддерживается");
-    }
+        else System.out.println("Ваша версия OS не поддерживается");}
 
-//    public static void main(String[] args) {
-//
-//        int clientOS = 0;
-//        int clientDeviceYear = 2018;
-//        printRecommendationVersionOs(clientOS, clientDeviceYear);
-//    }
 
     // Задача 3
 
@@ -59,15 +46,7 @@ public class Main {
     }
 
     public static void printDeliveryDays(int days) {
-        System.out.printf("Потребуется дней: " + days);
-    }
-
-//    public static void main(String[] args) {
-//        int deliveryDistance = 65;
-//        int days = calculateDeliveryTime(deliveryDistance);
-//        printDeliveryDays(days);
-//    }
-
+        System.out.printf("Потребуется дней: " + days);}
 
 }
 
